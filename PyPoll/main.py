c48bf4d01b29bd644ec7a6dfa462295bfd1e678d
Vaishali91tanwar <57017683+Vaishali91tanwar@users.noreply.github.com>
2020-02-17 12:58:18 -0500

    Add files via upload
    Completed Script

---
+++ PyPoll/main.py
@@ -0,0 +1,82 @@
+import os
+import csv
+
+#Declaration of list and dictionary used for storing information
+votes=[]
+candidates=[]
+candidate_vote={}
+candidate_percentage={}
+
+#Calculating the total votes received 
+def total_votes(row):
+    votes.append(row[2])
+
+#Storing the unique candidates in a list and creating a dictionary with uique names as keys and initialising their vote count to zero
+def unique_candidates(list):
+    for i in range(len(list)):
+        if list[i] not in candidates:
+            candidates.append(list[i])
+            candidate_vote[list[i]]=0
+
+
+#Calculating the total votes received by each candidate and storing in candidate_vote dictionary
+def count_votes(list):
+    for i in range(len(list)):
+        for d in candidate_vote:
+            if list[i]==d:
+                candidate_vote[d]=int(candidate_vote[d])+1
+
+#Calculating the percentage of votes received by each candidate
+def percentage_vote(dict):
+    for d in dict:
+        candidate_percentage[d]=round(int(dict[d])/len(votes)*100,3)
+        
+
+
+#Determinig the winner's name
+def winner(dict):
+    max=0
+    for d in dict:
+        if dict[d]>max:
+            max=dict[d]                
+            name=d
+    return name
+
+
+#Reading the csv file
+csvpath= os.path.join("C:/Users/Divneet/desktop/Python_Assignment_3/Python-Challenge/PyPoll/03-Python_Instructions_PyPoll_Resources_election_data.csv")
+with open(csvpath,  newline="") as csvfile:
+    csvreader=csv.reader(csvfile)
+    csv_header=next(csvreader)
+    for row in csvreader:
+        total_votes(row)
+
+#Calling of functions
+unique_candidates(votes)
+count_votes(votes)
+percentage_vote(candidate_vote)
+winner_name=winner(candidate_vote)
+
+#Printing the result to the terminal
+print("Election Results")
+print("------------------------")
+print(f"Total Votes: {len(votes)}")
+print("------------------------")
+for d in candidate_vote:
+    print(f"{d}: {candidate_percentage[d]}% ({candidate_vote[d]})")
+print("------------------------")
+print(f"Winner: {winner_name}")
+
+
+
+#Writing the output to the text file textPyPoll
+file=open("C:/Users/Divneet/Desktop/Python_Assignment_3/Python-Challenge/PyPoll/textPyPoll.txt","w")
+string1="Election Results\n-----------------------\nTotal votes: "+str(len(votes))+"\n-----------------------\n"
+file.write(string1)
+for d in candidate_vote:
+    string2=d+": "+str(candidate_percentage[d])+"% ("+str(candidate_vote[d])+")\n"
+    file.write(string2)
+string3="-----------------------\n"+"Winner: "+winner_name
+file.write(string3)
+file.close()
+
